66e2bb36660cfc4da9ec369151c0062a18bcc1a6
Joel Durafourd <joel.durafourd@gmail.com>
2024-07-17 21:25:00 +0900

    created bookings controller, implemented bookings, added them to the user profile

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -6,4 +6,47 @@ class BookingsController < ApplicationController
   def show
     @booking = Booking.find(params[:id])
   end
+
+  def new
+    @booking = Booking.new
+    @lesson = Lesson.find(params[:lesson_id])
+    @booking.user = current_user
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @lesson = Lesson.find(params[:lesson_id])
+    @booking.lesson_id = @lesson.id
+    @booking.user = current_user
+    if @booking.save
+      redirect_to lesson_path(@lesson)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @booking = Booking.find(params[:id])
+  end
+
+  def update
+    @booking = Booking.find(params[:id])
+    if @booking.update(booking_params)
+      redirect_to booking_path(@booking)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to list_path(@booking.list), status: :see_other
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
+  end
 end
