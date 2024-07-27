19c998d89da8f7460e769aca42c486ea1b31c9a9
Nakia Wilson <n.wil@workmail.com>
2024-07-27 12:41:22 +0900

    add scroll to requests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,6 @@ class UsersController < ApplicationController
   def show
     # user profile page, can be any user referenced not just current user, see above comments
     @user = User.find(params[:id])
-    @bookings = @user.bookings.order(start_date: :desc)
+    @bookings = @user.bookings.order(start_date: :asc)
   end
 end
